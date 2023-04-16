bca228cf626b684961455583a420d09f047db2ab
Chris Castle <chrisjcastle93@gmail.com>
2023-04-16 16:41:27 +0200

    test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const { Configuration, OpenAIApi } = require("openai");
+const { resolve } = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,36 +11,62 @@ const io = socketIO(server);
 const port = process.env.PORT || 3000;
 
 const configuration = new Configuration({
-    organization: 'org-2I9LMS95JGRJvd124KDjPVdj',
-    apiKey: process.env.OPENAI_API_KEY,
+  organization: "org-2I9LMS95JGRJvd124KDjPVdj",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    const conversationHistory = [];
-    socket.on("sendMessage", async (message, callback) => {
-        try {
-            conversationHistory.push({ role: "user", content: message });
-            const completion = await openai.createChatCompletion({
-                model: "gpt-3.5-turbo",
-                messages: conversationHistory,
-            });
-            const response = completion.data.choices[0].message.content;
-            conversationHistory.push({ role: "assistant", content: response });
-            socket.emit("message", response);
-            callback();
-        } catch (error) {
-            console.error(error.toJSON());
-            callback("Error: Unable to connect to the chatbot");
-        }
-    });
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
+  const conversationHistory = [];
+  socket.on("sendMessage", async (message, callback) => {
+    try {
+      conversationHistory.push({ role: "user", content: message });
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: conversationHistory,
+      });
+      const response = completion.data.choices[0].message.content;
+      conversationHistory.push({ role: "assistant", content: response });
+      socket.emit("message", response);
+      callback();
+    } catch (error) {
+      console.error(error.toJSON());
+      callback("Error: Unable to connect to the chatbot");
+    }
+  });
+
+  socket.on("itineraryRequest", async (req, callback) => {
+    try {
+      // const prompt = `You are a local tour guide in Berlin. ${req.particpants} have asked for you to write an itinerary for them that lasts three hours and includes a visit to the TV Tower. They are interested in ${req.food ? "food" : ""} ${req.food && req.drink ? "and" : ""} ${req.drink ? "drink" : ""} so include that. Write it in bullet points.`;
+
+      // const completion = await openai.createCompletion({
+      //   model: "text-davinci-003",
+      //   prompt,
+      //   max_tokens: 500,
+      // });
+
+      // console.log(completion.data.choices[0].text);
+      const response = " \n" + "\n" + "• Arrive at Berlin TV Tower \n" + "• Ascend the Tower and enjoy the 360 degree views of Berlin (approx. 45 minutes) \n" + "• Head to Checkpoint Charlie and explore Cold War History (approx. 30 minutes) \n" + "• Have lunch at a local café or restaurant nearby (approx. 45 minutes) \n" + "• Visit Brandenburger Tor and learn about its historical significance (approx. 30 minutes)\n" + "• Finish the tour at the Memorial to the Murdered Jews of Europe (approx. 30 minutes)";
+      socket.emit("itineraryResponse", response);
+
+      conversationHistory.push({ role: "assistant", content: response + ". How should we change this itinerary for you?" });
+
+      socket.emit("message", "How should we change this itinerary for you?");
+
+      callback();
+    } catch (error) {
+      console.error(error.toJSON());
+      callback("Error: Unable to connect to the chatbot");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- public/script.js
+++ public/script.js
@@ -2,51 +2,81 @@
 
 const socket = io();
 
+const hideButton = document.getElementById("hide");
 const messageForm = document.getElementById("message-form");
 const messageInput = document.getElementById("message-input");
 const messages = document.getElementById("messages");
+const requestForm = document.getElementById("request-form");
+const itineraryResponse = document.getElementById("itinerary-response");
+
+function displayItinerary(message) {
+  itineraryResponse.innerText = message;
+}
 
 function displayMessage(role, message) {
-    const li = document.createElement("li");
-    li.innerHTML = `
+  const li = document.createElement("li");
+  li.innerHTML = `
             <div class="mt-2 flex flex-col ${role === "user" ? "text-right" : ""}">
                 <h3 class="text-sm font-medium">${role === "user" ? "You" : "Bot"}</h3>
                 <p class="text-sm text-gray-500">${message}</p>
               </div>`;
-    messages.appendChild(li);
-    messages.scrollTop = messages.scrollHeight;
+  messages.appendChild(li);
+  messages.scrollTop = messages.scrollHeight;
 }
 
 function loading() {
-    const li = document.createElement("li");
-    li.innerHTML = `<p id="loading">LOADING</p>`;
-    messages.appendChild(li);
-    messages.scrollTop = messages.scrollHeight;
+  const li = document.createElement("li");
+  li.innerHTML = `<p id="loading">LOADING</p>`;
+  messages.appendChild(li);
+  messages.scrollTop = messages.scrollHeight;
 }
 
 function removeLoading() {
-    document.getElementById("loading").remove();
-    messages.scrollTop = messages.scrollHeight;
+  document.getElementById("loading").remove();
+  messages.scrollTop = messages.scrollHeight;
 }
 
 messageForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const message = messageInput.value;
-    displayMessage("user", message); // Display user's message in the chat
-    loading();
+  const message = messageInput.value;
+  displayMessage("user", message); // Display user's message in the chat
+  loading();
 
-    socket.emit("sendMessage", message, (error) => {
-        if (error) {
-            return alert(error);
-        }
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return alert(error);
+    }
+    messageInput.value = "";
+    messageInput.focus();
+  });
+});
 
-        messageInput.value = "";
-        messageInput.focus();
-    });
+// hideButton.addEventListener("click", (e) => {
+//     messageForm.classList.add('hidden');
+// });
+requestForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const req = {
+    name: e.target.name.value,
+    participants: parseInt(e.target.participants.value),
+    food: e.target.food.checked,
+    drink: e.target.drink.checked,
+    comment: e.target.comment.value,
+  };
+
+  socket.emit("itineraryRequest", req, (error) => {
+    if (error) {
+      return alert(error);
+    }
+  });
+});
+
+socket.on("itineraryResponse", (itinerary) => {
+  displayItinerary(itinerary); //
 });
 
 socket.on("message", (message) => {
-    removeLoading();
-    displayMessage("assistant", message); // Display assistant's message in the chat
+//   removeLoading();
+  displayMessage("assistant", message); // Display assistant's message in the chat
 });
